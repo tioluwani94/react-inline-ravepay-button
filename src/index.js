950685520d776c603785cc2d9d6a440c0bb2fc55
Tioluwani Kolawole <kolawole8tiolu@gmail.com>
2018-09-03 12:11:10 +0100

    add gitignore file

--- src/index.js
+++ src/index.js
@@ -1,54 +1,87 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
-import "./styles.css";
+import './styles.css';
 class RavepayButton extends React.Component {
+  static defaultProps = {
+    email: '',
+    apiKey: '',
+    amount: '',
+    phoneNumber: '',
+    currency: 'NGN',
+    firstName: '',
+    lastName: '',
+    successCallback: () => {},
+    failureCallback: () => {},
+    closeCallback: () => {},
+    txref: '',
+  };
+  static propTypes = {
+    email: PropTypes.string,
+    apiKey: PropTypes.string,
+    amount: PropTypes.string,
+    phoneNumber: PropTypes.string,
+    currency: PropTypes.string,
+    firstName: PropTypes.string,
+    lastName: PropTypes.string,
+    successCallback: PropTypes.func,
+    failureCallback: PropTypes.func,
+    closeCallback: PropTypes.func,
+    txref: PropTypes.string,
+  };
   onClick = () => {
     const {
       email,
       apiKey,
       amount,
       phoneNumber,
-      currency = "NGN",
+      firstName,
+      lastName,
+      currency,
       successCallback,
-      failureCallback
+      failureCallback,
+      closeCallback,
+      transactionRef,
     } = this.props;
-    var x = getpaidSetup({
+    var x = window.getpaidSetup({
       PBFPubKey: apiKey,
       customer_email: email,
       amount: amount,
       customer_phone: phoneNumber,
+      customer_firstname: firstName,
+      customer_lastname: lastName,
       currency: currency,
-      payment_method: "both",
-      txref: "rave-123456",
-      onclose: function() {},
+      payment_method: 'both',
+      txref: transactionRef,
+      onclose: closeCallback,
       callback: function(response) {
-        var txref = response.tx.txRef; // collect flwRef returned and pass to a 					server page to complete status check.
-        console.log("This is the response returned after a charge", response);
+        var txref = response.tx.txRef; // collect flwRef returned and pass to a server page to complete status check.
+        console.log('This is the response returned after a charge', response);
         if (
-          response.tx.chargeResponseCode == "00" ||
-          response.tx.chargeResponseCode == "0"
+          response.tx.chargeResponseCode === '00' ||
+          response.tx.chargeResponseCode === '0'
         ) {
-          successCallback();
+          successCallback(txref);
         } else {
           failureCallback();
         }
 
         x.close(); // use this to close the modal immediately after payment.
-      }
+      },
     });
   };
   componentWillMount() {
     if (!!window.RavePay === false) {
-      let ravepay = document.createElement("script");
+      let ravepay = document.createElement('script');
       ravepay.src =
-        "https://api.ravepay.co/flwv3-pug/getpaidx/api/flwpbf-inline.js";
+        'https://api.ravepay.co/flwv3-pug/getpaidx/api/flwpbf-inline.js';
       document.body.appendChild(ravepay);
     }
   }
   componentDidMount() {
     if (!!window.RavePay) {
-      console.log("loaded");
+      console.log('loaded');
     }
   }
   render() {
@@ -64,10 +97,12 @@ function App() {
 
       <RavepayButton
         render={onClick => <button onClick={onClick}>Pay</button>}
+        apiKey="FLWPUBK-faa8d835dfadbaaf5d8c397c29f5b38c-X"
+        email="test.tioluwani@gmail.com"
       />
     </div>
   );
 }
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
